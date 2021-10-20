0b4e73eae126ddcc9f671055addd4047201576ca
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-10-21 00:23:01 +0200

    fixed indendations

--- inireader.hpp
+++ inireader.hpp
@@ -14,307 +14,307 @@
 namespace fs = std::filesystem;
 
 namespace ini {
-    class Parser {
-    public:
-        Parser()
-        {
-            parsed_ = std::make_unique<Parsed>(); // avoid nullptr exceptions when parse hasn't been called
+  class Parser {
+  public:
+    Parser()
+    {
+      parsed_ = std::make_unique<Parsed>(); // avoid nullptr exceptions when parse hasn't been called
+    }
+
+    /// Opens file at given path and parses it
+    /**
+     * @param file_name Path to ini file to parse
+     * @return Opened file successfully
+     * @note Calling this function will reset the parsed data
+     */
+    bool Parse(const fs::path& file_name) {
+      std::fstream reader(file_name);
+      return Parse(reader);
+    }
+
+    /// Reads file and parsed it
+    /**
+     * @param file Open file stream with file to parse
+     * @return Opened file successfully
+     * @note Calling this function will reset the parsed data
+     */
+    bool Parse(std::fstream& file) {
+      // reset previous parse data if present
+      parsed_ = std::make_unique<Parsed>();
+      last_parse_error_.clear();
+
+      if (!file.is_open()) {
+        assert(file.is_open());
+        last_parse_error_ = "Couldn't open file";
+        return false;
+      }
+
+      std::vector<std::string> lines;
+      std::string buffer;
+      while (std::getline(file, buffer)) {
+        lines.emplace_back(buffer);
+      }
+      lines.shrink_to_fit(); // since the amount of lines will stay the same save some memory
+
+      for (auto&& line : lines)
+        ParseLine(line);
+
+      return true;
+    }
+
+    /**
+     * @return Stringified version of parsed data
+     */
+    std::string stringify() {
+      std::stringstream stringified;
+
+      // root kv
+      for (auto&& kv : parsed_->root)
+        stringified << kv.first << '=' << kv.second << "\n";
+
+      stringified << "\n"; // line between sections
+      // normal kv
+      for (auto&& section : parsed_->sections) {
+        stringified << '[' << section.first << ']' << "\n";
+        for (auto&& kv: section.second.entries)
+          stringified << kv.first << '=' << kv.second << "\n";
+        stringified << "\n"; // line between sections
+      }
+
+      return stringified.str();
+    }
+
+    /// Get a kv which is located before the first section
+    /**
+     * @param key
+     * @return Value of kv
+     */
+    std::string GetDefault(const std::string& key) {
+      auto entry = parsed_->root.find(key);
+
+      if (entry != parsed_->root.end()) {
+        return entry->second;
+      }
+      return {};
+    }
+
+    /**
+     * @param key
+     * @param val
+     * @note Overwrites value with key if already exists
+     */
+    void AddKVDefault(const std::string& key, const std::string& val) {
+      parsed_->root[key] = val;
+    }
+
+    /**
+     *
+     * @param section
+     * @param key
+     * @param val
+     * @note Overwrites value with key if already exists
+     */
+    void AddKV(const std::string& section, const std::string& key, const std::string& val) {
+      if (!section.empty()) {
+        auto entry = parsed_->sections.find(section);
+
+        if (entry != parsed_->sections.end()) {
+          entry->second.entries[key] = val;
+        } else {
+          Entries new_section;
+          new_section.entries[key] = val;
+          parsed_->sections[section] = new_section;
         }
+      } else {
+        AddKVDefault(key, val);
+      }
+    }
 
-        /// Opens file at given path and parses it
-        /**
-         * @param file_name Path to ini file to parse
-         * @return Opened file successfully
-         * @note Calling this function will reset the parsed data
-         */
-        bool Parse(const fs::path& file_name) {
-            std::fstream reader(file_name);
-            return Parse(reader);
+    /// Remove all key,values from the default section
+    void RemoveDefault() {
+      parsed_->root.clear();
+    }
+
+    /// Remove a kv from the default section
+    /**
+     * @param key
+     */
+    void RemoveKVDefault(const std::string& key) {
+      auto entry = parsed_->root.find(key);
+
+      if (entry != parsed_->root.end())
+        parsed_->root.erase(entry);
+    }
+
+    /// Remove a specific kv from a section
+    /**
+     * @param section
+     * @param key
+     */
+    void RemoveKV(const std::string& section, const std::string& key) {
+      auto entry = parsed_->sections.find(section);
+
+      if (entry != parsed_->sections.end()) {
+        auto entry_kv = entry->second.entries.find(key);
+
+        if (entry_kv != entry->second.entries.end())
+          entry->second.entries.erase(entry_kv);
+      }
+    }
+
+    /// Remove a section and all it's key,values
+    /**
+     * @param section
+     */
+    void RemoveSection(const std::string& section) {
+      auto entry = parsed_->sections.find(section);
+
+      if (entry != parsed_->sections.end())
+        parsed_->sections.erase(entry);
+    }
+
+    /**
+     * @return Has a parse error occurred
+     */
+    bool HasParseError() {
+      return !last_parse_error_.empty();
+    }
+
+    /**
+     * @return The last parse error
+     */
+    std::string GetParseError() {
+      return last_parse_error_;
+    }
+
+
+    using Entries_t = std::unordered_map<std::string, std::string>;
+
+    // has to be in a struct so that the [] operator can be implemented
+    struct Entries {
+      Entries_t entries;
+
+      /**
+       * @param key
+       * @return Value of key in section
+       */
+      std::string operator[](const std::string& key) {
+        auto entry = entries.find(key);
+
+        if (entry != entries.end()) {
+          return entry->second;
         }
-
-        /// Reads file and parsed it
-        /**
-         * @param file Open file stream with file to parse
-         * @return Opened file successfully
-         * @note Calling this function will reset the parsed data
-         */
-        bool Parse(std::fstream& file) {
-            // reset previous parse data if present
-            parsed_ = std::make_unique<Parsed>();
-            last_parse_error_.clear();
-
-            if (!file.is_open()) {
-                assert(file.is_open());
-                last_parse_error_ = "Couldn't open file";
-                return false;
-            }
-
-            std::vector<std::string> lines;
-            std::string buffer;
-            while (std::getline(file, buffer)) {
-                lines.emplace_back(buffer);
-            }
-            lines.shrink_to_fit(); // since the amount of lines will stay the same save some memory
-
-            for (auto&& line : lines)
-                ParseLine(line);
-
-            return true;
-        }
-
-				/**
-				 * @return Stringified version of parsed data
-				 */
-        std::string stringify() {
-            std::stringstream stringified;
-
-            // root kv
-            for (auto&& kv : parsed_->root)
-                stringified << kv.first << '=' << kv.second << "\n";
-
-            stringified << "\n"; // line between sections
-            // normal kv
-            for (auto&& section : parsed_->sections) {
-                stringified << '[' << section.first << ']' << "\n";
-                for (auto&& kv: section.second.entries)
-                    stringified << kv.first << '=' << kv.second << "\n";
-                stringified << "\n"; // line between sections
-            }
-
-            return stringified.str();
-        }
-
-        /// Get a kv which is located before the first section
-        /**
-         * @param key
-         * @return Value of kv
-         */
-        std::string GetDefault(const std::string& key) {
-            auto entry = parsed_->root.find(key);
-
-            if (entry != parsed_->root.end()) {
-                return entry->second;
-            }
-            return {};
-        }
-
-        /**
-         * @param key
-         * @param val
-         * @note Overwrites value with key if already exists
-         */
-        void AddKVDefault(const std::string& key, const std::string& val) {
-            parsed_->root[key] = val;
-        }
-
-        /**
-         *
-         * @param section
-         * @param key
-         * @param val
-         * @note Overwrites value with key if already exists
-         */
-        void AddKV(const std::string& section, const std::string& key, const std::string& val) {
-            if (!section.empty()) {
-                auto entry = parsed_->sections.find(section);
-
-                if (entry != parsed_->sections.end()) {
-                    entry->second.entries[key] = val;
-                } else {
-                    Entries new_section;
-                    new_section.entries[key] = val;
-                    parsed_->sections[section] = new_section;
-                }
-            } else {
-                AddKVDefault(key, val);
-            }
-        }
-
-				/// Remove all key,values from the default section
-        void RemoveDefault() {
-            parsed_->root.clear();
-        }
-
-				/// Remove a kv from the default section
-				/**
-				 * @param key
-				 */
-        void RemoveKVDefault(const std::string& key) {
-            auto entry = parsed_->root.find(key);
-
-            if (entry != parsed_->root.end())
-                parsed_->root.erase(entry);
-        }
-
-				/// Remove a specific kv from a section
-				/**
-				 * @param section
-				 * @param key
-				 */
-        void RemoveKV(const std::string& section, const std::string& key) {
-            auto entry = parsed_->sections.find(section);
-
-            if (entry != parsed_->sections.end()) {
-                auto entry_kv = entry->second.entries.find(key);
-
-                if (entry_kv != entry->second.entries.end())
-                    entry->second.entries.erase(entry_kv);
-            }
-        }
-
-				/// Remove a section and all it's key,values
-				/**
-				 * @param section
-				 */
-        void RemoveSection(const std::string& section) {
-            auto entry = parsed_->sections.find(section);
-
-            if (entry != parsed_->sections.end())
-                parsed_->sections.erase(entry);
-        }
-
-				/**
-				 * @return Has a parse error occurred
-				 */
-        bool HasParseError() {
-            return !last_parse_error_.empty();
-        }
-
-				/**
-				 * @return The last parse error
-				 */
-        std::string GetParseError() {
-            return last_parse_error_;
-        }
-
-
-        using Entries_t = std::unordered_map<std::string, std::string>;
-
-        // has to be in a struct so that the [] operator can be implemented
-        struct Entries {
-            Entries_t entries;
-
-						/**
-						 * @param key
-						 * @return Value of key in section
-						 */
-            std::string operator[](const std::string& key) {
-                auto entry = entries.find(key);
-
-                if (entry != entries.end()) {
-                    return entry->second;
-                }
-                return {};
-            }
-        };
-        using Sections_t = std::unordered_map<std::string, Entries>;
-
-				/**
-				 * @param section
-				 * @return Section with all it's entries
-				 */
-        Parser::Entries operator[](const std::string& section) {
-            auto entry = parsed_->sections.find(section);
-
-            if (entry != parsed_->sections.end()) {
-                return entry->second;
-            }
-            return {};
-        }
-
-    private:
-        struct Entry {
-            const std::string key;
-            const std::string value;
-        };
-
-        struct Parsed {
-            std::string current_section;
-            Sections_t sections;
-            Entries_t root;
-        };
-
-        std::string last_parse_error_;
-        std::unique_ptr<Parsed> parsed_;
-
-    private:
-        #define TRIM_STR(str, c) trim_r(trim(str, c), c)
-
-        void ParseLine(std::string& line) {
-            if (line.empty())
-                return;
-
-            RemoveComment(line);
-						if (line.empty())
-							return; // don't process the line any further when empty
-
-            auto entry = GetEntry(line);
-            if (!entry.key.empty() && !entry.value.empty()) {
-                if (!parsed_->current_section.empty()) {
-                    AddKV(parsed_->current_section, entry.key, entry.value);
-                } else {
-                    // no section has been found yet so write to root
-                    AddKVDefault(entry.key, entry.value);
-                }
-                return;
-            }
-
-            std::string section = GetSection(line);
-            if (!section.empty()) {
-                parsed_->current_section = std::move(section);
-                return;
-            }
-        }
-
-        static void RemoveComment(std::string& line) {
-            std::regex pattern(R"((^\;.*)|(^\#.*)|(.*[^\\];)|(.*[^\\]#))");
-            std::smatch match;
-
-            if (std::regex_search(line, match, pattern)) {
-                // full string match
-                if (match[0].str() == line)
-                    line.clear();
-                else
-                    line.erase(match[0].str().size() - 1, line.back());
-            }
-        }
-
-        static Entry GetEntry(const std::string& line) {
-            std::regex pattern(R"((.*)= ?(.*))");
-            std::smatch match;
-
-            if (std::regex_search(line, match, pattern)) {
-                return {
-                    TRIM_STR(match[1].str(), ' '),
-                    TRIM_STR(match[2].str(), ' ')
-                };
-            }
-            return {"",""};
-        }
-
-        static std::string GetSection(const std::string& line) {
-            std::regex pattern(R"(\[(.*)\])");
-            std::smatch match;
-
-            if (std::regex_search(line, match, pattern)) {
-                return TRIM_STR(match[1].str(), ' ');
-            }
-
-            return {};
-        }
-
-        static inline std::string trim(std::string str, char trim_c) {
-	        while(str.front() == trim_c) {
-						str.erase(0, 1);
-					}
-          return str;
-        }
-
-        static inline std::string trim_r(std::string str, char trim_c) {
-					while (str.back() == trim_c) {
-						str.pop_back();
-					}
-					return str;
-        }
+        return {};
+      }
     };
+    using Sections_t = std::unordered_map<std::string, Entries>;
+
+    /**
+     * @param section
+     * @return Section with all it's entries
+     */
+    Parser::Entries operator[](const std::string& section) {
+      auto entry = parsed_->sections.find(section);
+
+      if (entry != parsed_->sections.end()) {
+        return entry->second;
+      }
+      return {};
+    }
+
+  private:
+    struct Entry {
+      const std::string key;
+      const std::string value;
+    };
+
+    struct Parsed {
+      std::string current_section;
+      Sections_t sections;
+      Entries_t root;
+    };
+
+    std::string last_parse_error_;
+    std::unique_ptr<Parsed> parsed_;
+
+  private:
+#define TRIM_STR(str, c) trim_r(trim(str, c), c)
+
+    void ParseLine(std::string& line) {
+      if (line.empty())
+        return;
+
+      RemoveComment(line);
+      if (line.empty())
+        return; // don't process the line any further when empty
+
+      auto entry = GetEntry(line);
+      if (!entry.key.empty() && !entry.value.empty()) {
+        if (!parsed_->current_section.empty()) {
+          AddKV(parsed_->current_section, entry.key, entry.value);
+        } else {
+          // no section has been found yet so write to root
+          AddKVDefault(entry.key, entry.value);
+        }
+        return;
+      }
+
+      std::string section = GetSection(line);
+      if (!section.empty()) {
+        parsed_->current_section = std::move(section);
+        return;
+      }
+    }
+
+    static void RemoveComment(std::string& line) {
+      std::regex pattern(R"((^\;.*)|(^\#.*)|(.*[^\\];)|(.*[^\\]#))");
+      std::smatch match;
+
+      if (std::regex_search(line, match, pattern)) {
+        // full string match
+        if (match[0].str() == line)
+          line.clear();
+        else
+          line.erase(match[0].str().size() - 1, line.back());
+      }
+    }
+
+    static Entry GetEntry(const std::string& line) {
+      std::regex pattern(R"((.*)= ?(.*))");
+      std::smatch match;
+
+      if (std::regex_search(line, match, pattern)) {
+        return {
+          TRIM_STR(match[1].str(), ' '),
+          TRIM_STR(match[2].str(), ' ')
+        };
+      }
+      return {"",""};
+    }
+
+    static std::string GetSection(const std::string& line) {
+      std::regex pattern(R"(\[(.*)\])");
+      std::smatch match;
+
+      if (std::regex_search(line, match, pattern)) {
+        return TRIM_STR(match[1].str(), ' ');
+      }
+
+      return {};
+    }
+
+    static inline std::string trim(std::string str, char trim_c) {
+      while(str.front() == trim_c) {
+        str.erase(0, 1);
+      }
+      return str;
+    }
+
+    static inline std::string trim_r(std::string str, char trim_c) {
+      while (str.back() == trim_c) {
+        str.pop_back();
+      }
+      return str;
+    }
+  };
 }
--- test/test.cpp
+++ test/test.cpp
@@ -7,113 +7,113 @@ struct TestCtx;
 inline TestCtx* g_testctx{};
 
 struct TestCtx {
-	TestCtx() {
-		g_testctx = this;
-	}
+  TestCtx() {
+    g_testctx = this;
+  }
 
-	~TestCtx() {
-		g_testctx = nullptr;
-	}
+  ~TestCtx() {
+    g_testctx = nullptr;
+  }
 
-	ini::Parser ini_file;
+  ini::Parser ini_file;
 };
 
 TEST(Parse, NoError) {
-	EXPECT_EQ(g_testctx->ini_file.HasParseError(), false);
+  EXPECT_EQ(g_testctx->ini_file.HasParseError(), false);
 }
 
 TEST(Parse, DefaultSection) {
-	EXPECT_STREQ(g_testctx->ini_file.GetDefault("default section value").c_str(), "test value");
+  EXPECT_STREQ(g_testctx->ini_file.GetDefault("default section value").c_str(), "test value");
 }
 
 TEST(Parse, Section1) {
-	auto section = g_testctx->ini_file["Section 1"];
-	EXPECT_STREQ(section["Option 1"].c_str(), "value 1");
-	EXPECT_STREQ(section["Option 2"].c_str(), "value 2");
-	EXPECT_STREQ(section["oPtion 1"].c_str(), "value 2\\ \\ \\");
+  auto section = g_testctx->ini_file["Section 1"];
+  EXPECT_STREQ(section["Option 1"].c_str(), "value 1");
+  EXPECT_STREQ(section["Option 2"].c_str(), "value 2");
+  EXPECT_STREQ(section["oPtion 1"].c_str(), "value 2\\ \\ \\");
 }
 
 TEST(Parse, Numbers) {
-	auto section = g_testctx->ini_file["Numbers"];
-	EXPECT_STREQ(section["num"].c_str(), "-1285");
-	EXPECT_STREQ(section["num_bin"].c_str(), "0b01101001");
-	EXPECT_STREQ(section["num_hex"].c_str(), "0x12ae,0xAc2B");
-	EXPECT_STREQ(section["num_oct"].c_str(), "01754");
-	EXPECT_STREQ(section["float1"].c_str(), "-124.45667356");
-	EXPECT_STREQ(section["float2"].c_str(), "+4.1234565E+45");
-	EXPECT_STREQ(section["float3"].c_str(), "412.34565e45");
-	EXPECT_STREQ(section["float4"].c_str(), "-1.1245864E-6");
+  auto section = g_testctx->ini_file["Numbers"];
+  EXPECT_STREQ(section["num"].c_str(), "-1285");
+  EXPECT_STREQ(section["num_bin"].c_str(), "0b01101001");
+  EXPECT_STREQ(section["num_hex"].c_str(), "0x12ae,0xAc2B");
+  EXPECT_STREQ(section["num_oct"].c_str(), "01754");
+  EXPECT_STREQ(section["float1"].c_str(), "-124.45667356");
+  EXPECT_STREQ(section["float2"].c_str(), "+4.1234565E+45");
+  EXPECT_STREQ(section["float3"].c_str(), "412.34565e45");
+  EXPECT_STREQ(section["float4"].c_str(), "-1.1245864E-6");
 }
 
 TEST(Parse, Other) {
-	auto section = g_testctx->ini_file["Other"];
-	EXPECT_STREQ(section["bool1"].c_str(), "1");
-	EXPECT_STREQ(section["bool2"].c_str(), "on");
-	EXPECT_STREQ(section["bool3"].c_str(), "f");
+  auto section = g_testctx->ini_file["Other"];
+  EXPECT_STREQ(section["bool1"].c_str(), "1");
+  EXPECT_STREQ(section["bool2"].c_str(), "on");
+  EXPECT_STREQ(section["bool3"].c_str(), "f");
 }
 
 TEST(Add, Default) {
-	g_testctx->ini_file.AddKVDefault("testv", "hi");
-	g_testctx->ini_file.GetDefault("testv");
-	EXPECT_STREQ(g_testctx->ini_file.GetDefault("testv").c_str(), "hi");
+  g_testctx->ini_file.AddKVDefault("testv", "hi");
+  g_testctx->ini_file.GetDefault("testv");
+  EXPECT_STREQ(g_testctx->ini_file.GetDefault("testv").c_str(), "hi");
 }
 
 TEST(Add, Kv) {
-	g_testctx->ini_file.AddKV("addedsection", "test", "value");
-	EXPECT_STREQ(g_testctx->ini_file["addedsection"]["test"].c_str(), "value");
+  g_testctx->ini_file.AddKV("addedsection", "test", "value");
+  EXPECT_STREQ(g_testctx->ini_file["addedsection"]["test"].c_str(), "value");
 }
 
 TEST(Remove, Default) {
-	g_testctx->ini_file.RemoveKVDefault("testv");
-	EXPECT_STRNE(g_testctx->ini_file.GetDefault("testv").c_str(), "hi");
-	g_testctx->ini_file.RemoveDefault();
-	EXPECT_STRNE(g_testctx->ini_file.GetDefault("default section value").c_str(), "test value");
+  g_testctx->ini_file.RemoveKVDefault("testv");
+  EXPECT_STRNE(g_testctx->ini_file.GetDefault("testv").c_str(), "hi");
+  g_testctx->ini_file.RemoveDefault();
+  EXPECT_STRNE(g_testctx->ini_file.GetDefault("default section value").c_str(), "test value");
 }
 
 TEST(Remove, Kv) {
-	g_testctx->ini_file.RemoveKV("addedsection", "test");
-	EXPECT_STRNE(g_testctx->ini_file["addedsection"]["test"].c_str(), "value");
+  g_testctx->ini_file.RemoveKV("addedsection", "test");
+  EXPECT_STRNE(g_testctx->ini_file["addedsection"]["test"].c_str(), "value");
 }
 
 TEST(Remove, Section) {
-	g_testctx->ini_file.AddKV("addedsection", "test", "value");
-	g_testctx->ini_file.RemoveSection("addedsection");
-	EXPECT_STRNE(g_testctx->ini_file["addedsection"]["test"].c_str(), "value");
+  g_testctx->ini_file.AddKV("addedsection", "test", "value");
+  g_testctx->ini_file.RemoveSection("addedsection");
+  EXPECT_STRNE(g_testctx->ini_file["addedsection"]["test"].c_str(), "value");
 }
 
 int main(int argc, char** argv) {
-	constexpr const char* testfile = "default section value = test value ; section less\n"
-	                                 "\n"
-	                                 "[Section 1]\n"
-	                                 "; comment\n"
-	                                 "# comment2\n"
-	                                 "Option 1 = value 1                     ; option 'Option 1' has value 'value 1'\n"
-	                                 "Option 2 =  value 2                     # option 'Option 2' has value 'value 2'\n"
-	                                 "oPtion 1    =  value 2\\ \\ \\          ; option 'oPtion 1' has value ' value 2   ', 'oPtion 1' and 'Option 1' are different\n"
-	                                 "\n"
-	                                 "[Numbers]\n"
-	                                 "num = -1285\n"
-	                                 "num_bin = 0b01101001\n"
-	                                 "num_hex = 0x12ae,0xAc2B\n"
-	                                 "num_oct = 01754\n"
-	                                 "\n"
-	                                 "float1 = -124.45667356\n"
-	                                 "float2 = +4.1234565E+45\n"
-	                                 "float3 = 412.34565e45\n"
-	                                 "float4 = -1.1245864E-6\n"
-	                                 "\n"
-	                                 "[Other]\n"
-	                                 "bool1 = 1\n"
-	                                 "bool2 = on\n"
-	                                 "bool3=f";
+  constexpr const char* testfile = "default section value = test value ; section less\n"
+                                   "\n"
+                                   "[Section 1]\n"
+                                   "; comment\n"
+                                   "# comment2\n"
+                                   "Option 1 = value 1                     ; option 'Option 1' has value 'value 1'\n"
+                                   "Option 2 =  value 2                     # option 'Option 2' has value 'value 2'\n"
+                                   "oPtion 1    =  value 2\\ \\ \\          ; option 'oPtion 1' has value ' value 2   ', 'oPtion 1' and 'Option 1' are different\n"
+                                   "\n"
+                                   "[Numbers]\n"
+                                   "num = -1285\n"
+                                   "num_bin = 0b01101001\n"
+                                   "num_hex = 0x12ae,0xAc2B\n"
+                                   "num_oct = 01754\n"
+                                   "\n"
+                                   "float1 = -124.45667356\n"
+                                   "float2 = +4.1234565E+45\n"
+                                   "float3 = 412.34565e45\n"
+                                   "float4 = -1.1245864E-6\n"
+                                   "\n"
+                                   "[Other]\n"
+                                   "bool1 = 1\n"
+                                   "bool2 = on\n"
+                                   "bool3=f";
 
-	std::ofstream writer("test.ini");
-	writer << testfile;
-	writer.close();
+  std::ofstream writer("test.ini");
+  writer << testfile;
+  writer.close();
 
-	TestCtx test_ctx;
-	test_ctx.ini_file.Parse("test.ini");
+  TestCtx test_ctx;
+  test_ctx.ini_file.Parse("test.ini");
 
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
