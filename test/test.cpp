bdf3ae97e35b6b5d946c707d3bda82fcc7b4328b
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2023-01-23 22:33:50 +0100

    Fix tests on non win32 platforms

--- test/test.cpp
+++ test/test.cpp
@@ -145,11 +145,19 @@ TEST(Conversion, UTF8) {
 
   std::u16string hello_world_u16{ini::conversion::utility::DecodeUTF(hello_world)};
   EXPECT_EQ(hello_world, ini::conversion::utility::EncodeUTF(hello_world_u16));
+  #ifdef _WIN32
   EXPECT_EQ(hello_world_u16.length(), 13); // 13 bytes, 9 characters
+  #else
+  EXPECT_EQ(hello_world_u16.length(), 9); // 9 bytes, 9 characters
+  #endif
 
   std::u32string hello_world_u32{ini::conversion::utility::DecodeUTF<char32_t>(hello_world)};
   EXPECT_EQ(hello_world, ini::conversion::utility::EncodeUTF<char32_t>(hello_world_u32));
+  #ifdef _WIN32
   EXPECT_EQ(hello_world_u32.length(), 13); // 13 bytes, 9 characters
+  #else
+  EXPECT_EQ(hello_world_u32.length(), 9); // 9 bytes, 9 characters
+  #endif
 }
 
 int main(int argc, char** argv) {
