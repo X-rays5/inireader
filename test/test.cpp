1a3fb6fd3ef061ec9a19b4c13c34af00a4432395
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2023-01-23 22:25:20 +0100

    Add tests for UTF8 encoding and decoding

--- test/test.cpp
+++ test/test.cpp
@@ -140,6 +140,18 @@ TEST(Edit, Reference) {
   EXPECT_FALSE(g_testctx->ini_file["edit_ref"].HasValue("test_num"));
 }
 
+TEST(Conversion, UTF8) {
+  std::string hello_world{ u8"hello, 世界" };
+
+  std::u16string hello_world_u16{ini::conversion::utility::DecodeUTF(hello_world)};
+  EXPECT_EQ(hello_world, ini::conversion::utility::EncodeUTF(hello_world_u16));
+  EXPECT_EQ(hello_world_u16.length(), 13); // 13 bytes, 9 characters
+
+  std::u32string hello_world_u32{ini::conversion::utility::DecodeUTF<char32_t>(hello_world)};
+  EXPECT_EQ(hello_world, ini::conversion::utility::EncodeUTF<char32_t>(hello_world_u32));
+  EXPECT_EQ(hello_world_u32.length(), 13); // 13 bytes, 9 characters
+}
+
 int main(int argc, char** argv) {
   constexpr const char* testfile = "default section value = test value ; section less\n"
                                    "\n"
